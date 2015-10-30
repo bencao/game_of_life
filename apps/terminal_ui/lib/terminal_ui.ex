86c857fd7becdc95846adaf8bea411d44cfd0b3d
BenCao <zcao@freewheel.tv>
2015-10-30 14:07:45 +0000

    a graceful stop for terminal

--- apps/terminal_ui/lib/terminal_ui.ex
+++ apps/terminal_ui/lib/terminal_ui.ex
@@ -1,7 +1,15 @@
 defmodule TerminalUI do
-  use Application
+  def start do
+    sup_pid = TerminalUI.Supervisor.start_link
 
-  def start(_type, _args) do
-    TerminalUI.Supervisor.start_link
+    receive do
+      :stop ->
+        Process.exit(sup_pid, :shutdown)
+        :ok
+    end
+  end
+
+  def stop(pid) do
+    send pid, :stop
   end
 end
